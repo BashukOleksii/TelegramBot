9a2d27b06eaa09dc7acc1b7fb9fd00613f31f632
BashukOleksii <oleksiichort@gmail.com>
2025-12-15 00:58:13 +0200

    __

--- TelegramBot_MinimalAPI/Program.cs
+++ TelegramBot_MinimalAPI/Program.cs
@@ -19,9 +19,8 @@ app.Urls.Add($"http://0.0.0.0:{port}");
 app.MapPost("/telegram/update", async (Update update, TelegramBotClient botClient) =>
 {
     if (update.Message?.Text is not null)
-    {
         await botClient.SendMessage(update.Message.Chat.Id, "Привіт!");
-    }
+    
 });
 
 app.Run();
